9bdeab05735fa4d86a39f270c50cf933ccf285a9
johnb8005 <johnb8005@users.noreply.github.com>
2021-05-12 12:11:58 +0000

    deploy: f7c9f8c26c9578d90456aae07ed7649e34685330

--- src/home.js
+++ src/home.js
@@ -3,7 +3,7 @@ import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
 import React from "../_snowpack/pkg/react.js";
 import Salary from "./salary/index.js";
 const ghUrl = "https://github.com/nexys-admin/payroll-switzerland";
-const sha = __SNOWPACK_ENV__.SNOWPACK_GIT_SHA || "arandomsha";
+const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
 const ghUrlSha = `${ghUrl}/commit/${sha}`;
 export default () => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Payroll Switzerland"), /* @__PURE__ */ React.createElement(Salary, null), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("a", {
   href: ghUrl
