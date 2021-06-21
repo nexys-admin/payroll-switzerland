cc24e2cf85216d3fc4ad49bf53e12668565e9af4
Johan <johan@macair2020>
2021-06-21 18:21:15 +0200

    displaymonth

--- src/salary/index.tsx
+++ src/salary/index.tsx
@@ -10,6 +10,92 @@ import * as Municipality from "../geo/municipality";
 
 import BaseSummary from "./base-summary";
 
+enum Month {
+  January = 1,
+  February = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12,
+}
+
+const monthList: { id: number; name: string }[] = Object.values(Month)
+  .filter((x) => typeof x !== "string")
+  .map((x) => {
+    if (typeof x === "string") {
+      throw Error("should not be here");
+    } else {
+      return { id: x, name: Month[x] };
+    }
+  });
+
+interface MonthData {
+  year: number;
+  month: Month;
+  name: string;
+}
+
+const FormMonth = () => {
+  const [form, setForm] = React.useState<MonthData>({
+    year: 2021,
+    month: 1, //0,
+    name: "Johan",
+  });
+
+  return (
+    <>
+      <F.Form>
+        <F.Wrapper label={"Year"}>
+          <F.Input.Number
+            value={form.year}
+            onChange={(year) => setForm({ ...form, year })}
+          />
+        </F.Wrapper>
+
+        <F.Wrapper label="Month">
+          <F.Select<Month>
+            value={form.month}
+            options={monthList}
+            onChange={(month) => setForm({ ...form, month })}
+          />
+        </F.Wrapper>
+
+        <F.Wrapper label={"Name"}>
+          <F.Input.Text
+            value={form.name}
+            onChange={(name) => setForm({ ...form, name })}
+          />
+        </F.Wrapper>
+      </F.Form>
+
+      {form.month > 0 && form.name !== "" && <DisplayMonth data={form} />}
+    </>
+  );
+};
+
+const DisplayMonth = ({ data }: { data: MonthData }) => {
+  const start = new Date(data.year, data.month - 1, 1);
+  const end = new Date(
+    new Date(data.year, data.month, 1).getTime() - 1000 * 3600 * 24
+  );
+
+  const month = Month[data.month];
+  return (
+    <>
+      <h3>{data.name}</h3>
+      <p>
+        Du {start.getDate()} {month} au {end.getDate()} {month} {data.year}
+      </p>
+    </>
+  );
+};
+
 export default (): JSX.Element => {
   const [brut, setBrut] = React.useState<number | "">("");
   const [lpp, setLpp] = React.useState<number | "">("");
@@ -43,6 +129,8 @@ export default (): JSX.Element => {
       {brut !== "" && lpp !== "" && (
         <Table lppYearly={lpp} base={brut} deductions={U.getDeductions(brut)} />
       )}
+
+      <FormMonth />
     </>
   );
 };
