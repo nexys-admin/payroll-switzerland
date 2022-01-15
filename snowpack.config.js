5975648fe7e9cec248f3cb247ad4a643b4d17d5e
John B <johan@nexys.ch>
2022-01-15 14:28:01 +0000

    change snowpack config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,4 +1,12 @@
+// Snowpack Configuration File
+/** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  routes: [{ match: "routes", src: ".*", dest: "/index.html" }],
-  exclude: ["**/node_modules/**/*", "LICENSE"],
+  mount: {
+    public: { url: "/", static: true },
+    src: "/",
+  },
+  exclude: ["**/*.test.ts", "**/*.test.tsx", "lib/test/test-utils.tsx","**/node_modules/**/*", "LICENSE"],
+  routes: [
+    { match: "routes", src: ".*", dest: "/index.html" },
+  ],
 };
