d9bc80eb3b6755ce8ea93eaa4d020b6604c4faf8
johnb8005 <johnb8005@users.noreply.github.com>
2022-01-15 10:56:28 +0000

    deploy: df490922e1701e0ba2cec12783a0a8602a1e6c7c

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,5 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "5446786c89202c149527c2b73f85f4102e123a5a";
-export const SNOWPACK_PUBLIC_VERSION = "v0.0.1";
+export const SNOWPACK_PUBLIC_GIT_SHA = "df490922e1701e0ba2cec12783a0a8602a1e6c7c";
+export const SNOWPACK_PUBLIC_VERSION = "v0.0.2";
 export const SNOWPACK_PUBLIC_URL = "/payroll-switzerland";
 export const MODE = "development";
 export const NODE_ENV = "development";
--- src/home.js
+++ src/home.js
@@ -3,12 +3,12 @@ import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
 import React from "../_snowpack/pkg/react.js";
 import Salary from "./salary/index.js";
 const ghUrl = "https://github.com/nexys-admin/payroll-switzerland";
-const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "arandomsha";
-const ghUrlSha = `${ghUrl}/commit/${sha}`;
+const version = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_VERSION || "unset_version";
+const ghUrlVersion = `${ghUrl}/releases/tag/${version}`;
 export default () => /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("h1", null, "Payroll Switzerland"), /* @__PURE__ */ React.createElement(Salary, null), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("a", {
   href: ghUrl
 }, /* @__PURE__ */ React.createElement("i", {
   className: "fa fa-code"
 }), " Source"), " available under MIT license. Contributions are welcome."), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("small", null, /* @__PURE__ */ React.createElement("a", {
-  href: ghUrlSha
-}, sha.slice(0, 15)))));
+  href: ghUrlVersion
+}, version))));
