73d9de02bc8bf584716209d798c2b11c6407f6aa
johnb8005 <johnb8005@users.noreply.github.com>
2022-01-15 10:52:41 +0000

    deploy: 3ef1ca67bd459390594e4492ecdcd2345040e468

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "075ef2023906cabb3ba16b95fb03ceabe24dce9a";
+export const SNOWPACK_PUBLIC_GIT_SHA = "3ef1ca67bd459390594e4492ecdcd2345040e468";
 export const SNOWPACK_PUBLIC_URL = "/payroll-switzerland";
 export const MODE = "development";
 export const NODE_ENV = "development";
