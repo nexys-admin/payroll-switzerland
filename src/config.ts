d8a18a5e07bfbee13338155f08530d4e9eb802fe
John B <johan@nexys.ch>
2022-01-15 11:58:15 +0100

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+const ghUrl = "https://github.com/nexys-admin/payroll-switzerland";
+
+export const sha = import.meta.env.SNOWPACK_PUBLIC_GIT_SHA || "unset_sha";
+export const ghUrlSha = `${ghUrl}/commit/${sha}`;
+
+export const version = import.meta.env.SNOWPACK_PUBLIC_VERSION || "unset_version";
+export const ghUrlVersion= `${ghUrl}/releases/tag/${version}`;
